92022d28e83d37efa7a61f754d3bbc29715fb89f
LinWin-Cloud <2382546858@qq.com>
2023-05-13 10:37:52 +0800

    Kylin language

--- Kylin/src/main/java/com/kylin/Runtime/MainRuntime.java
+++ Kylin/src/main/java/com/kylin/Runtime/MainRuntime.java
@@ -9,10 +9,7 @@ import program.vm.BaseRuntime;
 
 
 import javax.script.*;
-import java.io.BufferedReader;
-import java.io.PipedReader;
-import java.io.PipedWriter;
-import java.io.Reader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -45,12 +42,13 @@ public class MainRuntime {
     }
     public void exec(String source_code,String new_var) throws Exception {
         String[] words = source_code.split(" ");
-        String e = this.ImportantCharset.get(words[0]);
+        String e = this.ImportantCharset.get(words[0].trim());
+        //System.out.println(this.ImportantCharset);
 
         if (words[0].startsWith("//")) {
             return;
         }
-        if (words[0].equals("#defined")) {
+        if (words[0].equals("#defined") || (e != null && e.equals("#defined"))) {
             try
             {
                 String key = words[1];
@@ -91,6 +89,51 @@ public class MainRuntime {
         {
             BaseLib.javascript(this.code , codeLine,this);
             return;
+        }
+        if (source_code.startsWith("#head")) {
+            try {
+                String head = source_code.substring(5).trim();
+                if (head.startsWith("<") && head.endsWith(">"))
+                {
+                    String input = head.substring(1,  head.length() - 1);
+                    input = input.replace("{head}","../head");
+                    File file = new File(input);
+                    if (!file.isFile() || !file.canRead()) {
+                        MainRuntime.sendRuntimeError("Can not open: "+file.getAbsolutePath() , codeLine);
+                        System.exit(1);
+                    }
+                    else {
+                        FileReader fileReader = new FileReader(file);
+                        BufferedReader bufferedReader = new BufferedReader(fileReader);
+                        while (true) {
+                            String line = bufferedReader.readLine();
+                            if (line == null){
+                                break;
+                            }
+                            if (line.startsWith("#defined")) {
+                                try
+                                {
+                                    String key = line.split(" ")[1];
+                                    String value = line.split(" ")[2];
+                                    this.ImportantCharset.put(value,key);
+                                }
+                                catch (Exception exception){
+                                    MainRuntime.sendSyntaxError(exception.getMessage() ,codeLine);
+                                }
+                            }
+                        }
+                        bufferedReader.close();
+                        fileReader.close();
+                        return;
+                    }
+                }
+                else {
+                    MainRuntime.sendSyntaxError(source_code , codeLine);
+                }
+            }
+            catch (Exception exception){
+                MainRuntime.sendSyntaxError(exception.getMessage() , codeLine);
+            }
         }
         else {
             BaseRuntime baseRuntime = new BaseRuntime();
