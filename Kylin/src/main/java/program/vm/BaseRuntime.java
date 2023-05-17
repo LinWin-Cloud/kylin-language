cdb65e4e8cf0fb2585b9480fd15ff648e190d359
LinWin-Cloud <2382546858@qq.com>
2023-05-17 07:11:35 -0400

    Kylin

--- Kylin/src/main/java/program/vm/BaseRuntime.java
+++ Kylin/src/main/java/program/vm/BaseRuntime.java
@@ -4,8 +4,7 @@ import com.kylin.Runtime.Expression;
 import com.kylin.Runtime.ListExpression;
 import com.kylin.Runtime.MainRuntime;
 
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -27,16 +26,23 @@ public class BaseRuntime {
                 BaseLib.javascript(new ArrayList<>(Arrays.asList(s)) , line, mainRuntime);
                 return "";
             }
-            if (code.startsWith("write")) {
-                String getInput = subContent;
-                ListExpression listExpression = new ListExpression();
-                String[] inputContent = listExpression.get_list_expression(getInput,line , mainRuntime);
-                try{
-                    String path = inputContent[1];
-
-                }catch (Exception exception) {
-                    MainRuntime.sendSyntaxError(exception.getMessage() , line);
+            if (code.startsWith("shell")) {
+                String execCommand = Expression.getExString(subContent , line, mainRuntime);
+                boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+                Process process;
+                if (isWindows) {
+                    process = Runtime.getRuntime().exec("cmd /c "+execCommand);
                 }
+                else {
+                    process = Runtime.getRuntime().exec("/bin/sh -c "+execCommand);
+                }
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String GetLine;
+                while ((GetLine = reader.readLine()) != null) {
+                    System.out.println(GetLine);
+                }
+                reader.close();
+                return String.valueOf(process.waitFor());
             }
             else {
                 MainRuntime.sendRuntimeError("[ERR] "+code , line+1);
@@ -49,9 +55,14 @@ public class BaseRuntime {
             return null;
         }
     }
-    public static String[] getFunctionInput(String code, int line) {
-        String getInput = code.substring(code.indexOf("(") + 1, code.lastIndexOf(")"));
-        ListExpression listExpression = new ListExpression();
-        String[] inputContent = listExpression.get_list_expression(getInput,line , mainRuntime);
+    public static String[] getFunctionInput(String code, int line, MainRuntime mainRuntime) {
+        try{
+            String getInput = code.substring(code.indexOf("(") + 1, code.lastIndexOf(")"));
+            ListExpression listExpression = new ListExpression();
+            return listExpression.get_list_expression(getInput,line , mainRuntime);
+        }catch (Exception exception) {
+            MainRuntime.sendSyntaxError(exception.getMessage() , line);
+            return null;
+        }
     }
 }
