3ad492f95d061df6c0360a688e8a196d488cfe49
LinWin-Cloud <2382546858@qq.com>
2023-05-27 08:18:37 -0400

    src

--- Kylin/src/main/java/com/kylin/Runtime/ForOptimizer.java
+++ Kylin/src/main/java/com/kylin/Runtime/ForOptimizer.java
@@ -9,8 +9,19 @@ public class ForOptimizer {
         StringBuffer stringBuffer = new StringBuffer();
         ArrayList<String> stringArrayList = new ArrayList<>(Arrays.asList(code.split("\n")));
         HashMap<String , Integer> indexMap = new HashMap<>();
-
-
+        String[] split = code.split("\n");
+        for (int i = 0 ; i < split.length ; i++)
+        {
+            if (split[i].startsWith("for")) {
+                indexMap.put(split[i] , i);
+                continue;
+            }
+            if (split[i].startsWith("end_for")) {
+                indexMap.put(split[i] , i);
+                continue;
+            }
+        }
+        System.out.println(code);
         return stringBuffer.toString();
     }
     private void ForNext(String code) {
