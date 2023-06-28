2031dc990b387ba205931c863451612b88ff6371
LinWin-Cloud <2382546858@qq.com>
2023-06-27 23:30:41 -0400

    build_project.sh

--- build_project.sh
+++ build_project.sh
@@ -3,5 +3,7 @@
 find src/. | grep .java > source.txt
 javac -d ./out/production/kylin_ke/ @source.txt
 cd ./out/production/kylin_ke/
-jar -cvfm kylin_language.jar ../../../bin/META-INF/MANIFEST.MF *
+jar -cfm kylin_language.jar ../../../bin/META-INF/MANIFEST.MF *
 cp kylin_language.jar ../../../bin/
+cd ../../../bin/
+java -jar kylin_language.jar ./hello.ky
