2cd93daae9a12b5917546fd782875549f25e5b5a
LinWin-Cloud <2382546858@qq.com>
2023-07-06 13:41:50 +0800

    resource code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 import KylinException.KylinRuntimeException;
 
+/**
+ * 家人们，谁懂啊，这个项目前前后后重构好几次，不过这次确实设计的很好了，反正少走了很多弯路
+ * 这个项目中有很多代码是 ChatGPT 写的，如果看到操蛋的代码那一定是人工智能的错误 [迫真!]
+ */
+
 public class Main {
     public static void main(String[] args) {
         try {
--- src/Program/ImportLib.java
+++ src/Program/ImportLib.java
@@ -1,8 +1,46 @@
 package Program;
 
+import java.io.File;
+
 public class ImportLib {
     public static void lib_import(String expression , KylinRuntime kylinRuntime) throws Exception
     {
-        
+
+    }
+
+    public static File searchFile(String basePath, String searchString) {
+    	/**
+	     * 这段代码是ChatGPT写的，我大概检查了一下，确实可以运行
+	     *
+	     * The ChatGPT was wrote these code and these code can run very well.
+	     */    
+	     
+	     File baseDir = new File(basePath);
+
+        if (!baseDir.isDirectory()) {
+            return null;
+        }
+
+        File[] files = baseDir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().equals(searchString + ".ky")) {
+                    return file;
+                } else if (file.isDirectory()) {
+                    File result = searchFile(file.getAbsolutePath(), searchString);
+                    if (result != null) {
+                        return result;
+                    }
+                }
+            }
+
+            for (File file : files) {
+                if (file.isFile() && file.getName().startsWith(searchString)) {
+                    return file;
+                }
+            }
+        }
+
+        return null;
     }
 }
