9cdff36c5d191bb9401db91d081b6b8f4681919d
LinWin-Cloud <2382546858@qq.com>
2023-07-07 06:40:54 -0400

    resource code

--- src/Program/ImportLib.java
+++ src/Program/ImportLib.java
@@ -1,15 +1,21 @@
 package Program;
 
+import main.baseFunction;
+
 import java.io.File;
 
 public class ImportLib {
     public static void lib_import(String expression , KylinRuntime kylinRuntime) throws Exception
     {
-        String path = main.mainApp.jarDirectory + "/";
+        String path = main.mainApp.jarDirectory + "/../lib/";
         File result = searchFile(path, expression);
         if (result != null) {
             KylinRuntime import_kylin = new KylinRuntime(result.getName());
-            
+            import_kylin.code = baseFunction.getScript(result.getAbsolutePath()); //把代码加载进入运行环境
+            import_kylin.run();
+            for (KylinFunction kylinFunction : import_kylin.FunctionMap.values()) {
+                kylinRuntime.FunctionMap.put(result.getName()+"."+kylinFunction.name , kylinFunction);
+            }
         }else {
             throw new Exception("No module: "+expression);
         }
@@ -19,10 +25,10 @@ public class ImportLib {
     	/**
 	     * 这段代码是ChatGPT写的，我大概检查了一下，确实可以运行
 	     *
-	     * The ChatGPT was wrote these code and these code can run very well.
+	     * The ChatGPT was written these code and these code can run very well.
 	     */    
 	     
-	     File baseDir = new File(basePath);
+        File baseDir = new File(basePath);
 
         if (!baseDir.isDirectory()) {
             return null;
