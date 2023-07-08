9a8a1f5e7cdad199d086873f9967d19e1e701219
LinWin-Cloud <2382546858@qq.com>
2023-07-08 03:39:36 -0400

    resource code

--- src/Program/KyLinClass.java
+++ src/Program/KyLinClass.java
@@ -1,10 +1,27 @@
 package Program;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class KyLinClass {
-    private String name;
+    public String name;
     private boolean isPublic = false;
+    private KyLinRuntime kyLinRuntime;
     public HashMap<String , KyLinFunction> functionHashMap = new HashMap<>();
+    public HashMap<String , KyLinValue> valueHashMap = new HashMap<>();
+    public ArrayList<String> code = new ArrayList<>();
 
+    public KyLinClass(String name)
+    {
+        this.name = name;
+        kyLinRuntime = new KyLinRuntime(name);
+    }
+
+    public void init_class() throws Exception
+    {
+        for (int i = 0 ; i < this.code.size() ; i++)
+        {
+            
+        }
+    }
 }
