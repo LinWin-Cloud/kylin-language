ff3da2f036a072b8c77153cd2a3e900a8e6d2c24
LinWin-Cloud <2382546858@qq.com>
2023-07-08 03:30:51 -0400

    resource code

--- src/Program/KyLinClass.java
+++ src/Program/KyLinClass.java
@@ -1,4 +1,10 @@
 package Program;
 
+import java.util.HashMap;
+
 public class KyLinClass {
+    private String name;
+    private boolean isPublic = false;
+    public HashMap<String , KyLinFunction> functionHashMap = new HashMap<>();
+
 }
