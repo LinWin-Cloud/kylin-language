5a74df8c5a704e72bf88ea3d99998347559440eb
LinWin-CLoud <2382546858@qq.com>
2024-01-30 14:58:06 +0800

    git_do.sh

---
+++ git_do.sh
@@ -0,0 +1,10 @@
+git add bin
+git commit -m 'the command and the binary file'
+git add src
+git commit -m 'the resource code: Java'
+git add out
+git commit -m 'out of the class file: Java Class'
+git add head
+git commit -m 'kylin head lib: *.kyh'
+git add release
+git commit -m 'release of the jar file'
