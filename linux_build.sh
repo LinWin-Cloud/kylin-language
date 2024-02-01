a6bb20c95ff67f87b9a95e576a72b1cd4065b5a1
LinWin-Cloud <2382546858@qq.com>
2024-02-01 20:36:37 +0800

    linux_build.sh

--- linux_build.sh
+++ linux_build.sh
@@ -5,5 +5,6 @@ javac -d ./out/production/kylin_ke/ @source.txt
 cd ./out/production/kylin_ke/
 jar -cfm kylin_language.jar ../../../bin/META-INF/MANIFEST.MF *
 cp kylin_language.jar ../../../release/
+echo '[INFO] build ok.'
 cd ../../../bin/
 ./kylin ../hello.ky
