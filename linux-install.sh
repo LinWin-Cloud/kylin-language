83542a605a4bf1621acb5e6986d3fa6817ffa551
LinWin-Cloud <2382546858@qq.com>
2024-03-07 11:57:22 +0800

    linux-install

--- linux-install.sh
+++ linux-install.sh
@@ -5,3 +5,6 @@ sudo cp -r ./* /usr/kylin
 sudo echo '/usr/kylin/kylin $1 $2' > /bin/kylin
 sudo chmod +x /bin/kylin
 sudo chmod 777 /usr/kylin/* -R
+
+sudo echo '/usr/kylin/bin/kpt $1 $2 $3 $4 > /bin/kpt'
+sudo chmod +x /bin/kpt
