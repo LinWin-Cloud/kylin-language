887c79bdf32298a59d7d45ff47496cb4e5539080
LinWin-Cloud <2382546858@qq.com>
2024-03-12 20:34:46 +0800

    the command and the binary file

--- src/main/mainApp.java
+++ src/main/mainApp.java
@@ -26,7 +26,7 @@ public class mainApp {
     public static HashMap<String , String> import_lib_value = new HashMap<>();
     public static HashMap<String , KyLinValue> all_kylin_value_pointer = new HashMap<>();
     public static ConcurrentHashMap<String , Thread> all_kylin_thread_map = new ConcurrentHashMap<>();
-    public static double version = 4.3;
+    public static double version = 4.4;
     public static HashMap<String , KyLinFunction> all_kylin_function_pointer = new HashMap<>();
     public static String version_type = "Public";
 
